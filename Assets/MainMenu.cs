4559d7a75be9f2601d03316971d51850c6881615
Naluxdee <arthit.phan@bumail.net>
2024-04-27 00:25:53 +0700

    menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credit()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
